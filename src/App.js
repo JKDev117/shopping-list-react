9dfa27920e7e35a145e409ff9f8e3f9a781a9773
Jae Kim <jckim81@yahoo.com>
2020-02-04 14:54:12 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AddItemForm from './AddItemForm';
+import ShoppingList from './ShoppingList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    shoppingItems: [
+      /* put stub items in here for testing */
+      { name: 'apples', checked: false },
+      { name: 'oranges', checked: true },
+      { name: 'bread', checked: false },
+    ]
+  };
+
+  handleDeleteItem(item){
+    console.log('handle delete item called', item)
+  }
+
+  handleCheckItem(item){
+    console.log('handle check item called', item)
+  }
+
+  render() {
+    return (
+      <>
+        <header>
+          <h1>Shopping List</h1>
+        </header>
+        <main>
+          {/* */}
+          <section>
+            <AddItemForm />
+          </section>
+          
+          {/* */}
+          <section>
+            <ShoppingList
+              items={this.state.shoppingItems}
+              //Okay
+              onDeleteItem={this.handleDeleteItem}
+              onCheckItem={this.handleCheckItem}
+            />
+          </section>
+        </main>
+      </>
+    )
+  }
 }
+
+
 
 export default App;
